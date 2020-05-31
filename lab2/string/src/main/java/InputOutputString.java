2612d3832e5554ec0482594d6e108afb1a47f8aa
ines4 <ines4348@gmail.com>
2020-05-31 14:53:34 +0300

    Comments corrected

--- lab2/string/src/main/java/InputOutputString.java
+++ lab2/string/src/main/java/InputOutputString.java
@@ -8,13 +8,14 @@ public class InputOutputString {
     }
 
     public String GetHtmlString() {
-        Scanner scanner = new Scanner(System.in);
         String tempString = "";
-        while (scanner.hasNextLine()) {
-            try {
-                tempString += scanner.nextLine();
-            } catch (NoSuchElementException error) {
-                System.out.print("No line was found!");
+        try(Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                try {
+                    tempString += scanner.nextLine();
+                } catch (NoSuchElementException error) {
+                    System.out.print("No line was found!");
+                }
             }
         }
         return tempString;
